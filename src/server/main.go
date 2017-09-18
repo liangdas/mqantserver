4bd0d1e039d9cda0dfc3aa5bf845122630cba52a
liangdas <1587790525@qq.com>
2017-09-18 17:15:41 +0800

    =

--- src/server/main.go
+++ src/server/main.go
@@ -35,13 +35,14 @@ func DefaultTracer()opentracing.Tracer{
 
 func main() {
 	app := mqant.CreateApp()
-	app.DefaultTracer(func()opentracing.Tracer {
-		if collector==nil{
-			collector=appdash.NewRemoteCollector("127.0.0.1:7701")
-			tracer=appdashtracer.NewTracer(collector)
-		}
-		return tracer
-	})
+	//先不用分布式跟踪服务了
+	//app.DefaultTracer(func()opentracing.Tracer {
+	//	if collector==nil{
+	//		collector=appdash.NewRemoteCollector("127.0.0.1:7701")
+	//		tracer=appdashtracer.NewTracer(collector)
+	//	}
+	//	return tracer
+	//})
 	//app.Route("Chat",ChatRoute)
 	app.Run(true, //只有是在调试模式下才会在控制台打印日志, 非调试模式下只在日志文件中输出日志
 		modules.MasterModule(),
